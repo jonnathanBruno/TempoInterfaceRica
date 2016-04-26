7b02baeded3580172ad6c12da8ba0c2c29e98d0b
jonnathan <jonnathanbruno100@hotmail.com>
2016-04-25 21:50:17 -0300

    grafico

--- controllers/IndexController.js
+++ controllers/IndexController.js
@@ -7,12 +7,76 @@ angular.module('TempoApp', ['services.WebServices'])
 		.success(function(data){
 			
 			$scope.retorno = angular.fromJson(data);
+			$scope.carregarGrafico($scope.retorno.previsoes);
 			$scope.previsoes = $scope.retorno.previsoes;
 			$scope.pageSize = 1;
 			$scope.pegarLatitudeELongitude($scope.retorno.cidade);
 		})
 		.error(function(data) {
 			alert("Ocorreu um erro!");
+		});
+	}
+	
+	$scope.carregarGrafico = function(previsoes){	
+		var previsoesTemp = [];
+		for(var i=0; i<previsoes.length; i++){
+			previsoesTemp[i] = previsoes[i].temperatura_max;
+		}
+		var previsoesTempOrdendas = previsoesTemp.sort(function(a, b){return b-a});
+		
+		var maiorValor = 0;
+		var tresMaiores = [];
+		tresMaiores[0] = previsoesTempOrdendas[0];
+		var aux=1;
+		
+		for(var u=1; u<previsoes.length; u++){
+			if(tresMaiores[aux-1] != previsoesTempOrdendas[u])
+				tresMaiores[aux++] = previsoesTempOrdendas[u];
+			if(aux==3)
+				break;
+		}
+		
+		var data = [];
+		var aux = 0;
+		var flag = 0;
+		for(var j=0; j<previsoesTempOrdendas.length; j++){
+			if(aux==3)
+				break;
+			if(previsoesTempOrdendas[j] == tresMaiores[aux]){
+				flag++;
+				if(previsoesTempOrdendas.length == j+1)
+					data[aux++] = flag;
+			}
+			else{
+				data[aux++] = flag;
+				flag = 1;
+			}
+		}
+		
+		var ctx = document.getElementById("myChart");
+		var data = {
+			labels: tresMaiores,
+			datasets: [
+				{
+					data: data,
+					backgroundColor: [
+						"#FF6384",
+						"#36A2EB",
+						"#FFCE56"
+					],
+					hoverBackgroundColor: [
+						"#FF6384",
+						"#36A2EB",
+						"#FFCE56"
+					]
+				}]
+		};
+		var myPieChart = new Chart(ctx,{
+			type: 'pie',
+			data: data,
+			animation:{
+				animateScale:true
+			}
 		});
 	}
 	
